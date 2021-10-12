658019c4f89d6dc95b133dbbf739d2683c53dd24
Rp317 <purajarikrussell@gmail.com>
2021-10-11 19:42:23 -0500

    2 files modified

--- index.tsx
+++ index.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 interface AppProps {}
 interface AppState {
   name: string;
@@ -11,30 +11,39 @@ class App extends Component<AppProps, AppState> {
     super(props);
   }
 
-
   //classname="..." for tailwind css
   //try to make something that connects all the things. (includes discord, gmail, github, spotify, youtube and more!)
   //*ambition intensifys*
   render() {
     return (
       <div>
-        
-        <div id="heading">
-          <h1 className="text-3xl text-green-400 text-center">SuperFit</h1>
-        </div>
-        <div id="paragraphs">
-          <h2>It's litteraly the all-in-one app to store all your apps!</h2>
-          <h2>Cutstom placements for your apps!</h2>
-          <h2>
-            Tired of diffrent themes for each app? How about have one cool theme
-            instead?
-          </h2>
-          <h2>You can intigrate gmail, discord, github, spotify, etc!</h2>
-        </div>
-        <div id="ending">
-          <h1 id="shit">Download now!</h1>
-          <h5>(please wait not yet avalible though)</h5>
-        </div>
+        <Tabs>
+          <TabList className="relative bg-green-500 font-extrabold">
+            <Tab>Who we are</Tab>
+            <Tab>download</Tab>
+          </TabList>
+          <TabPanel>
+            <div id="heading">
+              <h1 className="text-3xl text-green-400 text-center">SuperFit</h1>
+            </div>
+            <div id="paragraphs">
+              <h2>It's litteraly the all-in-one app to store all your apps!</h2>
+              <h2>Cutstom placements for your apps!</h2>
+              <h2>
+                Tired of diffrent themes for each app? How about have one cool
+                theme instead?
+              </h2>
+              <h2>You can intigrate gmail, discord, github, spotify, etc!</h2>
+            </div>
+            <div id="ending">
+              <h1 id="shit">Download now!</h1>
+              <h5>(please wait not yet avalible though)</h5>
+            </div>
+          </TabPanel>
+          <TabPanel>
+            <h2>Any content 2</h2>
+          </TabPanel>
+        </Tabs>
       </div>
     );
   }
